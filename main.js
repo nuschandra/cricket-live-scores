b28e6d1823b2a9068df4442996743b55e5cbfa3d
System Administrator <cviswana@visa.com>
2017-09-02 01:22:12 +0800

    removing jquery

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import $ from 'jquery';
 import {HashRouter as Router,Route,Link} from 'react-router-dom';
 
 ReactDOM.render(<Router>
